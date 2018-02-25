c4efb20ae013964b8a56693545ffa21e6b25d6f8
Inwadek <acceptance364@gmail.com>
2018-02-24 21:33:45 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,51 +1,21 @@
 let v=1;
 let c=true;
-self.addEventListener('fetch', function(event) {
+
+self.addEventListener('fetch',function(event){
   event.respondWith(
     caches.open('mysite-dynamic').then(function(cache){
-      cache.keys().then(function(keys){
-        keys.forEach(function(request,index,array){
-          if(request==v){
-            c=false;
-            return v;
-          }else{
-            cache.delete(request);
-          }
-        });
-        if(c){
-          cache.put(v,response.clone())
-          return response;
+      keys.forEach(function(request,index,array){
+        if(request==v){
+          c=false;
+          return v;
+        }else{
+          cache.delete(request);
         }
-      });
-    })
-  );
-});
-
-/*
-    caches.match(v).then(function(matches){
-      if(!matches){
-        fetch(event.request).then(function(response){
-          cache.put(v,response.clone());
-          
-          caches.open(v).then(function(cache) {
-            cache.keys().then(function(keys) {
-              keys.forEach(function(request, index, array) {
-                cache.delete(request);
-              });
-            });
-          })
-          
-          return response;
-        });
-      }else{
-        return match;
+      })
+      if(c){
+        cache.put(v,response.clone());
+        return response;
       }
-    });
-  );
+    })
+  )
 });
-*/
-/*
-self.addEventListener('fetch',function(event){
- event.respondWith(fetch(event.request.url));
-});
-*/
