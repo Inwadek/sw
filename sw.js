6c268febe3a03bd2b1066f4455bd19beee0888bb
Inwadek <acceptance364@gmail.com>
2018-02-24 21:55:21 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,12 @@
 let v='v';
-let c=true;
 
 self.addEventListener('fetch',function(event){
   event.respondWith(
-    caches.open('mysite-dynamic').then(function(cache){
-      cache.keys().then(function(keys){
-        keys.forEach(function(request,index,array){
-          if(request==v){
-            c=false;
-            return v;
-          }else{
-            cache.delete(request);
-          }
-        })
+    fetch(event.request).then(function(response){
+      caches.open('test').then(function(cache){
+        cache.put(request,response.clone());
+        return response;
       })
-      if(c){
-        fetch(event.request.url).then(function(response){
-          cache.put(v,response.clone());
-          return response;
-        })
-      }
     })
   )
 });
