f72cf8af66a38ecc4d682b6c6bea374334613861
Inwadek <acceptance364@gmail.com>
2018-02-24 22:35:16 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,6 +4,11 @@ self.addEventListener('fetch', function(event) {
     caches.open('test').then(function(cache) {
       return cache.match(v).then(function (response) {
         return response || fetch(event.request).then(function(response) {
+          cache.keys().then(function(keys){
+            keys.forEach(function(request,index,array){
+              cache.delete(request);
+            })
+          });
           cache.put(v, response.clone());
           return response;
         });
