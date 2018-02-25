8e416c9139e5b730e8b4ad48ce1ac6851ccedc5a
Inwadek <acceptance364@gmail.com>
2018-02-25 10:41:31 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,23 @@
+var v='v1';
+
 self.addEventListener('install',function(event){
   event.waitUntil(
-    caches.open('test').then(function(cache){
+    caches.open(v).then(function(cache){
       return cache.add('/sw/');
     })
   )
 });
+
+self.addEventListener('activate',function(event){
+  event.waitUntil(
+    return Promise.all(
+      caches.keys().map(function(cacheName){
+        if(cacheName!==v)return caches.delete(cacheName);
+      })
+    )
+  )
+})
+
 self.addEventListener('fetch',function(event){
   event.respondWith(
     caches.match(event.request).then(function(response){
