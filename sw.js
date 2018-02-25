2716a9a8984ab01d1d0444de0f5d2b6b0b3697d1
Inwadek <acceptance364@gmail.com>
2018-02-24 22:43:11 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-var v='2';
+var v='3';
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.open('test').then(function(cache) {
       return cache.match(v).then(function (response) {
-        return response || fetch(event.request).then(function(req) {
+        return response || fetch(event.request.url).then(function(req) {
           cache.keys().then(function(keys){
             keys.forEach(function(request,index,array){
               cache.delete(request);
