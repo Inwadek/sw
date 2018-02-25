d4491ddd251c3cc5370b8458b464b9170f603caf
Inwadek <acceptance364@gmail.com>
2018-02-25 10:01:12 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,12 @@
-var v='v3';
 self.addEventListener('fetch',function(event){
   event.respondWith(
     caches.match(event.request).then(function(request){
       return request || fetch(event.request)
     })
   )
-})
-self.addEventListener('install',function(event){
-  event.waitUntil(
-    caches.open(v).then(function(cache){
-      return cache.add('index.html')
-    })
-  )
-})
-self.addEventListener('activate',function(event){
-  event.waitUntil(
-    caches.keys().then(function(keyList){
-      return Promise.all(keyList.map(function(key,i){
-        if(key!==v){
-          return caches.delete(keyList[i])
-        }
-      }))
-    })
-  )
+});
+self.addEventListener('fetch',function(event){
+  caches.open('test').then(function(cache){
+    return cache.add('index.html');
+  })
 })
