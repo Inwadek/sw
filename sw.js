4d512601baec4ea82564e695a9531d6fed65c403
Inwadek <acceptance364@gmail.com>
2018-02-24 22:54:44 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,21 @@
-var v='5';
+var v='0';
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.open('test').then(function(cache) {
       return cache.match(v).then(function (response) {
-        return response || fetch(event.request.url).then(function(req) {
-          cache.keys().then(function(keys){
-            keys.forEach(function(request,index,array){
-              cache.delete(request);
+        if(response){
+          return response;
+        }else{
+          fetch(event.request.url).then(function(response){
+            cache.keys().then(function(keys){
+              keys.forEach(function(request,index,array){
+                cache.delete(request);
+              })
             })
-          });
-          cache.put(v, req.clone());
-          return req;
-        });
+            cache.put(v.response.clone());
+            return response;
+          })
+        }
       });
     })
   );
