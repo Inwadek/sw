af5d501b4c55fcc680418ccfcca7137dfdb29a50
Inwadek <acceptance364@gmail.com>
2018-02-24 21:24:23 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,49 @@
+let v=0;
+let c=true;
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.open('mysite-dynamic').then(function(cache) {
-      return cache.match(event.request).then(function (response) {
-        return response || fetch(event.request).then(function(response) {
-          cache.put(event.request, response.clone());
-          return response;
+    caches.open('mysite-dynamic').then(function(cache){
+      cache.keys().then(function(keys){
+        keys.forEach(function(request,index,array){
+          if(request==v){
+            c=false;
+            return v;
+          }else{
+            cache.delete(request);
+          }
         });
+        if(c){
+          cache.put(v,response.clone())
+          return response;
+        }
       });
-    })
+    });
   );
 });
 
+/*
+    caches.match(v).then(function(matches){
+      if(!matches){
+        fetch(event.request).then(function(response){
+          cache.put(v,response.clone());
+          
+          caches.open(v).then(function(cache) {
+            cache.keys().then(function(keys) {
+              keys.forEach(function(request, index, array) {
+                cache.delete(request);
+              });
+            });
+          })
+          
+          return response;
+        });
+      }else{
+        return match;
+      }
+    });
+  );
+});
+*/
 /*
 self.addEventListener('fetch',function(event){
  event.respondWith(fetch(event.request.url));
