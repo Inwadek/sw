3fd1bf614e24371ea3efb892ae1cf34bc165ef4e
Inwadek <acceptance364@gmail.com>
2018-03-21 12:24:13 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,47 +1,3 @@
-var CACHE_NAME = '7';
-// Files required to make this app work offline
-var REQUIRED_FILES = [
-  '/sw/',
-];
-
-self.addEventListener('install', function(event) {
-  // Perform install step:  loading each required file into cache
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        // Add all offline dependencies to the cache
-        return cache.addAll(REQUIRED_FILES);
-      })
-      .then(function() {
-      	// At this point everything has been cached
-        return self.skipWaiting();
-      })
-  );
-});
-
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return the response from the cached version
-        if (response) {
-          return response;
-        }
-
-        // Not in cache - return the result from the live server
-        // `fetch` is essentially a "fallback"
-        return fetch(event.request);
-      }
-    )
-  );
-});
-
-self.addEventListener('activate', function(event) {
-  // Calling claim() to force a "controllerchange" event on navigator.serviceWorker
-  event.waitUntil(self.clients.claim());
-});
-
-/*
 var v='4';
 self.addEventListener('install',function(event){
   event.waitUntil(
@@ -67,4 +23,3 @@ self.addEventListener('fetch',function(event){
     })
   )
 });
-*/
